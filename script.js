4a685d9240891243d4dd5eabd52f5baba9db371b
Manav Arora <manavaroratl225907@proton.me>
2025-04-23 16:05:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Simple form submission handling
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for contacting FREE GAMING STUDIOS! We will get back to you soon.');
+    // Here you can add code to actually send the form data to a server
+});
